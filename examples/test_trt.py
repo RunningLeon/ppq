b63ccc85adf1f68ff8768ef908dda68ba133cb80
RunningLeon <mnsheng@yeah.net>
2022-10-21 20:12:30 +0800

    update test trt

--- examples/test_trt.py
+++ examples/test_trt.py
@@ -19,68 +19,75 @@ def main():
     with open(args.config, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
         config = edict(config)
+
     WORKING_DIRECTORY = config.model.working_dir
     MMDEPLOY_DIR = config.mmdeploy_dir
     os.makedirs(WORKING_DIRECTORY, exist_ok=True)
     ONNX_MODEL_FILE = osp.join(WORKING_DIRECTORY, 'end2end.onnx')
+    TRT_FP32_ENGINE = osp.join(WORKING_DIRECTORY, 'end2end.engine')
+    TRT_INT8_ENGINE = osp.join(WORKING_DIRECTORY, 'end2end-int8.engine')
     DEPLOY_CFG_PATH = osp.join(config.mmdeploy_dir, config.model.deploy_cfg)
     DEPLOY_CFG_INT8_PATH = osp.join(config.mmdeploy_dir,
                                     config.model.deploy_cfg_int8)
     MODEL_CFG_PATH = osp.join(config.mmseg_dir, config.model.model_cfg)
-    TEST_IMAGE = osp.join(config.mmseg_dir, 'demo/demo.png')
+    if not osp.exists(ONNX_MODEL_FILE) or config.pipeline.torch2onnx:
+        TEST_IMAGE = osp.join(config.mmseg_dir, 'demo/demo.png')
+        PYTORCH_CHECKPOINT = config.model.checkpoint
+        # torch2onnx
+        cmd_lines = [
+            'python',
+            osp.join(MMDEPLOY_DIR, 'tools/deploy.py'),
+            DEPLOY_CFG_PATH,
+            MODEL_CFG_PATH,
+            PYTORCH_CHECKPOINT,
+            TEST_IMAGE,
+            '--device cuda:0',
+            f'--work-dir {WORKING_DIRECTORY}',
+        ]
+        log_path = osp.join(WORKING_DIRECTORY, 'torch2onnx.log')
+        run_cmd(cmd_lines, log_path)
+    if not osp.exists(TRT_FP32_ENGINE) or config.pipeline.test_trt_fp32:
+        # test trt fp32
+        cmd_lines = [
+            'python',
+            osp.join(MMDEPLOY_DIR,
+                     'tools/test.py'), DEPLOY_CFG_PATH, MODEL_CFG_PATH,
+            '--device cuda:0', f'--model {TRT_FP32_ENGINE}', '--metrics mIoU'
+        ]
+        log_path = osp.join(WORKING_DIRECTORY, 'test_trt_fp32.log')
+        run_cmd(cmd_lines, log_path)
 
-    PYTORCH_CHECKPOINT = config.model.checkpoint
-    # torch2onnx
-    cmd_lines = [
-        'python',
-        osp.join(MMDEPLOY_DIR, 'tools/deploy.py'),
-        DEPLOY_CFG_PATH,
-        MODEL_CFG_PATH,
-        PYTORCH_CHECKPOINT,
-        TEST_IMAGE,
-        '--device cuda:0',
-        f'--work-dir {WORKING_DIRECTORY}',
-    ]
-    log_path = osp.join(WORKING_DIRECTORY, 'torch2onnx.log')
-    run_cmd(cmd_lines, log_path)
+    if not osp.exists(TRT_INT8_ENGINE) or config.pipeline.test_trt_int8:
+        # test trt int8 of original trt
+        print('test original trt int8')
+        # create calib file
+        h5_calibe_file = config.calib.calibration_h5file
+        if not osp.exists(h5_calibe_file):
+            data_dir, _ = osp.split(h5_calibe_file)
+            os.makedirs(data_dir, exist_ok=True)
+            calib_dataloader = build_mmseg_dataloader(
+                MODEL_CFG_PATH, 'train', config.calib.calibration_file)
+            create_calib_input_data(h5_calibe_file, calib_dataloader)
 
-    # test trt fp32
-    cmd_lines = [
-        'python',
-        osp.join(MMDEPLOY_DIR, 'tools/test.py'), DEPLOY_CFG_PATH,
-        MODEL_CFG_PATH, '--device cuda:0',
-        f'--model {osp.join(WORKING_DIRECTORY, "end2end.engine")}',
-        '--metrics mIoU'
-    ]
-    log_path = osp.join(WORKING_DIRECTORY, 'test_trt_fp32.log')
-    run_cmd(cmd_lines, log_path)
+        cmd_lines = [
+            'python',
+            osp.join(MMDEPLOY_DIR, 'tools/onnx2tensorrt.py'),
+            DEPLOY_CFG_INT8_PATH, ONNX_MODEL_FILE,
+            osp.splitext(TRT_INT8_ENGINE)[0], f'--calib-file {h5_calibe_file}'
+        ]
+        log_path = osp.join(WORKING_DIRECTORY,
+                            'original_onnx2tensorrt-int8.log')
+        run_cmd(cmd_lines, log_path)
 
-    # test trt int8 of original trt
-    print('test original trt int8')
-    # create calib file
-    h5_calibe_file = config.calib.calibration_h5file
-    if not osp.exists(h5_calibe_file):
-        calib_dataloader = build_mmseg_dataloader(
-            config.model.model_cfg, 'train', config.calib.calibration_file)
-        create_calib_input_data(h5_calibe_file, calib_dataloader)
-    trt_int8_engine = osp.join(WORKING_DIRECTORY, 'end2end-int8.engine')
-    cmd_lines = [
-        'python',
-        osp.join(MMDEPLOY_DIR, 'tools/onnx2tensorrt.py'), DEPLOY_CFG_INT8_PATH,
-        ONNX_MODEL_FILE,
-        osp.splitext(trt_int8_engine)[0], f'--calib-file {h5_calibe_file}'
-    ]
-    log_path = osp.join(WORKING_DIRECTORY, 'original_onnx2tensorrt-int8.log')
-    run_cmd(cmd_lines, log_path)
-
-    cmd_lines = [
-        'python',
-        osp.join(MMDEPLOY_DIR,
-                 'tools/test.py'), DEPLOY_CFG_INT8_PATH, MODEL_CFG_PATH,
-        '--device cuda:0', f'--model {trt_int8_engine}', '--metrics mIoU'
-    ]
-    log_path = osp.join(WORKING_DIRECTORY, 'test_trt_int8.log')
-    run_cmd(cmd_lines, log_path)
+        cmd_lines = [
+            'python',
+            osp.join(MMDEPLOY_DIR,
+                     'tools/test.py'), DEPLOY_CFG_INT8_PATH, MODEL_CFG_PATH,
+            '--device cuda:0', f'--model {TRT_INT8_ENGINE}', '--metrics mIoU'
+        ]
+        log_path = osp.join(WORKING_DIRECTORY, 'test_trt_int8.log')
+        run_cmd(cmd_lines, log_path)
+    print('all done for test trt')
 
 
 if __name__ == '__main__':
